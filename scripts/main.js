5b6dc8ac50673979d401524280bbe475e194854d
Dan Herlihy <github@danherlihy.me>
2026-05-19 22:39:44 -0400

    Better rotating attributes

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,86 @@
+const eraseWord = (word, initialDelay) => {
+  const target = document.getElementById("typing");
+  const removeLetter = () => {
+    target.textContent = target.textContent.substring(0, target.textContent.length - 1);
+  }
+  let totalTypingTime = initialDelay;
+  for (let i = 0; i < word.length; i++) {
+    totalTypingTime += 100;
+    setTimeout(() => removeLetter(), totalTypingTime)
+  }
+  return totalTypingTime;
+}
+
+const gaussianRandom = (mean=0, stdev=1) => {
+    const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+    const v = Math.random();
+    const z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    // Transform to the desired mean and standard deviation:
+    const rand = z * stdev + mean;
+    return Math.max(rand, 20);
+}
+
+const TYPING_MEAN = 112;
+const TYPING_STDEV = 56;
+
+const typeWord = (word) => {
+  const target = document.getElementById("typing");
+  const addLetter = (char) => {
+    target.textContent = target.textContent + char;
+  }
+  
+  let totalTypingTime = 0;
+  for (let i = 0; i < word.length; i++) {
+    let charToType = word.charAt(i);
+    if (charToType === " ") {
+      charToType = "\xa0" // NBSP
+    }
+
+    totalTypingTime += gaussianRandom(TYPING_MEAN, TYPING_STDEV);
+    setTimeout(() => addLetter(charToType), totalTypingTime);
+  }
+  return totalTypingTime;
+}
+
 const configureAttributes = () => {
-  const subtitle = document.getElementById("main-subtitle");
-  const attributeSpan = document.getElementById("rotating-attribute");
-  let attributes = [
-    "Leader",
-    "Juggler",
+  const attributes = [
     "Totally not a Robot",
-    "Friend",
-    "Consultant",
+    '"Professional"',
     "Volunteer",
-    "DevOps Engineer",
     "Nittany Lion",
     "Problem Solver",
-    "Open-Source Contributer",
-    "Communicator",
     "Automator",
-    "Lifelong Learner",
+    "Learner",
     "Guitar Player",
-    "Data Engineer"
+    "Data Engineer",
+    "Full-Stack Developer",
+    "Nerd",
+    "NYT Spelling Bee Enthusiast"
   ];
-  let used = [];
+  const used = new Set();
 
-  function getTextWidth(text, font) {
-    // re-use canvas object for better performance
-    var canvas =
-      getTextWidth.canvas ||
-      (getTextWidth.canvas = document.createElement("canvas"));
-    var context = canvas.getContext("2d");
-    context.font = font;
-    var metrics = context.measureText(text);
-    return metrics.width;
+  const nextAttribute = () => {
+    if (attributes.length === used.size) {
+      used.clear();
+    }
+    while (true) {
+      const index = Math.floor(Math.random() * attributes.length);
+      const candidate = attributes[index];
+      if (!used.has(candidate)) {
+        used.add(candidate);
+        return candidate;
+      }
+    }
   }
 
-  const rotateAttribute = () => {
-    attributeSpan.classList.remove("visible");
-    attributeSpan.classList.add("hidden");
-    setTimeout(() => {
-      if (attributes.length === 0) {
-        attributes = [...used];
-        used = [];
-      }
-      const randomIndex = Math.floor(attributes.length * Math.random());
-      used.push(attributes[randomIndex]);
-      attributeSpan.innerText = attributes[randomIndex];
-      attributes = attributes.filter((_, i) => i !== randomIndex);
-      const maxWidth = Math.max(
-        document.documentElement.clientWidth || 0,
-        window.innerWidth || 0
-      );
-      const newWidth = 378 + attributeSpan.clientWidth;
-      const width = newWidth < maxWidth ? newWidth : maxWidth;
-      subtitle.style.width =  width + "px";
-      attributeSpan.classList.remove("hidden");
-      attributeSpan.classList.add("visible");
-    }, 3000);
-  };
+  const typeNextWord = () => {
+    const attribute = nextAttribute();
+    const typeTime = typeWord(attribute);
+    const totalTime = eraseWord(attribute, typeTime + 3000);
+    setTimeout(() => typeNextWord(), totalTime + 2000);
+  }
 
-  setTimeout(() => {
-    rotateAttribute();
-    setInterval(rotateAttribute, 10000);
-  }, 1500);
+  setTimeout(() => typeNextWord(), 2000);
   
 }
 
